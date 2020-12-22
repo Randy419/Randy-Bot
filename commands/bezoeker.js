0bd57a6fd0df5119bd83dee6efc2d23d4ae0b31b
Randy419 <71568554+Randy419@users.noreply.github.com>
2020-12-22 17:02:39 +0100

    Init
    d

--- commands/bezoeker.js
+++ commands/bezoeker.js
@@ -4,10 +4,10 @@ module.exports.run = async (client, message, args) => {
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("sorry jij kan dit niet");
 
     var noRoleUser = message.guild.member(message.mentions.user.ferst());
-    if(!noRoleUser) return message.channel.send("Geen Gebruiker met deze naam Gevonden")
+    if(!noRoleUser) return message.channel.send("Geen Gebruiker met deze naam Gevonden");
 
-    var role = message.guid.roles.cach.find(r => r.name === "Bezoekers")
-    if(!role) return message.channel.send("Geen rol met deze naam Gevonden")
+    var role = message.guid.roles.cach.find(r => r.name === "Bezoekers");
+    if(!role) return message.channel.send("Geen rol met deze naam Gevonden");
     
     for (let index = 0; index < noRoleUser._roles.length; index++) {
         const role = noRoleUser._roles[index];

e9e7244c5339d63ed7bd0e1ee2c71f9930930da9
Randy419 <71568554+Randy419@users.noreply.github.com>
2020-12-29 11:40:50 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -8,7 +8,23 @@ module.exports.run = async (client, message, args) => {
 
     if(!args[0]) return message.reply("Gebruik !prefix <prefix hier>");
 
-}
+    var prefixes = JSON.parse(fs.readFileSync("./prefixes.json"));
+    
+    prefixes[message.guild.id] = {
+        prefixes: args[0]
+    };
+
+    fs.writeFileSync("./prefixes.json", JSON.stringify(prefixes),(err) => {
+        if (err) console.log(err);
+
+    });
+
+   var  stringEmbed = new discord.RichEmbed()
+    .setcoler("f00")
+    .setTitle("prefix")
+    .setDescription('prefix aangepast naar ${args[0]}');
+    message.channel.send(stringEmbed);}
+    
 
 module.exports.help = {
     name: "prefix",
